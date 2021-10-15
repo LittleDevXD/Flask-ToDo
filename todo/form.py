33801a2e530877449d03f5347cb01d571d53a8f0
Sitt Lin Htet <sailinhtet142@gmail.com>
2021-10-15 21:43:51 +0630

    finish project

--- todo/form.py
+++ todo/form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from todo.models import User
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
@@ -8,6 +9,18 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Log in")
 
 class RegisterForm(FlaskForm):
+
+    # Custom Build
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError("Username already exists! Please choose another one.")
+
+    def validate_email(self, email_to_check):
+        email = User.query.filter_by(email=email_to_check.data).first()
+        if email:
+            raise ValidationError("There is already an account with this email address.")
+
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password1 = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
@@ -20,3 +33,13 @@ class LogoutForm(FlaskForm):
 class TodoForm(FlaskForm):
     task = StringField("Your Todo Here", validators=[DataRequired()])
     submit = SubmitField("Create Task")
+
+class CompleteForm(FlaskForm):
+    submit = SubmitField("Complete")
+
+class UncompleteForm(FlaskForm):
+    submit = SubmitField("Uncomplete")
+
+class DeleteForm(FlaskForm):
+    submit = SubmitField("Delete Task")
+
--- todo/routes.py
+++ todo/routes.py
@@ -1,7 +1,7 @@
 from todo import app, db
 from flask import render_template, redirect, url_for, flash, get_flashed_messages, request
-from todo.form import LoginForm, RegisterForm, TodoForm
-from todo.models import User, ToDo
+from todo.form import CompleteForm, DeleteForm, LoginForm, RegisterForm, TodoForm, UncompleteForm
+from todo.models import FinishToDo, User, ToDo
 from flask_login import login_user, logout_user, current_user, login_required
 
 @app.route("/")
@@ -13,16 +13,67 @@ def home_page():
 @login_required
 def todo_page():
     form = TodoForm()
-    if request.method == "POST":
-        task = ToDo(task=form.task.data)
-        task.user = current_user.id
-        db.session.add(task)
-        db.session.commit()
-        return redirect(url_for("todo_page"))
+    delete_form = DeleteForm()
+    complete_form = CompleteForm()
+    uncomplete_form = UncompleteForm()
 
+    if request.method == "POST":
+        # Adding initial Task
+        if form.validate_on_submit():
+            task = ToDo(task=form.task.data)
+            task.user = current_user.id
+            db.session.add(task)
+            db.session.commit()
+        
+        # Move the completed task to FinishToDo Database
+        if complete_form.validate_on_submit():
+            # Delete Completed Task from ToDo db
+            completed_tasks = request.form.get("completed_task")
+            c_task_obj = ToDo.query.filter_by(task=completed_tasks).first()
+            if c_task_obj:
+                # Transfer the task into FinishToDo db
+                task = c_task_obj.task
+                finished_task = FinishToDo(task=task)
+                finished_task.user = c_task_obj.user
+                # Commiting to Database
+                db.session.add(finished_task)
+                db.session.delete(c_task_obj)
+                db.session.commit()
+
+        # Move the uncompleted tasks back to ToDo Database
+        if uncomplete_form.validate_on_submit():
+            # Delete Completed Task from FinishToDo db
+            uncomplete_tasks = request.form.get("uncomplete_task")
+            print(uncomplete_tasks)
+            uc_task_obj = FinishToDo.query.filter_by(task=uncomplete_tasks).first()
+            
+            if uc_task_obj:
+                # Transfer the task into ToDo db
+                task = uc_task_obj.task
+                unfinished_task = ToDo(task=task)
+                unfinished_task.user = uc_task_obj.user
+                # Commiting to Database
+                db.session.add(unfinished_task)
+                db.session.delete(uc_task_obj)
+                db.session.commit()
+
+        # Delete the finished task from the database
+        if delete_form.validate_on_submit():
+            deleted_task = request.form.get("deleted_task")
+            d_task_obj = FinishToDo.query.filter_by(task=deleted_task).first()
+            if d_task_obj:
+                db.session.delete(d_task_obj)
+                db.session.commit()
+
+        return redirect(url_for("todo_page"))
+        
     if request.method == "GET":
         tasks = ToDo.query.filter_by(user=current_user.id)
-    return render_template("todo.html", form=form, tasks=tasks)
+        completed_tasks = FinishToDo.query.filter_by(user=current_user.id)
+
+    return render_template("todo.html", form=form, tasks=tasks, 
+                            delete_form=delete_form, complete_form=complete_form, 
+                            f_tasks=completed_tasks, uncomplete_form=uncomplete_form)
 
 # Login
 @app.route("/login", methods=["GET", "POST"])
@@ -51,7 +102,7 @@ def register_page():
         login_user(user)
         return redirect(url_for("todo_page"))
     elif form.errors != {}:
-        for error in form.errors:
+        for error in form.errors.values():
             flash(f"There was an error creating the user: {error}", category="danger")
     return render_template("register.html", form=form)
 
